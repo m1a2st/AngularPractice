dc1333b689a0d80822e97d2cd941be1b2a56cd4e
m1a2st <s7133700@gmail.com>
2022-11-12 16:51:51 +0800

    fix : 購物車商品刷新會消失>刷新後會暫存在storage裏面 分成兩種一個是sessionStorage 另一種是 localStorage 這次是使用localStorage

--- frontend/angular-ecommerce/src/app/service/cart.service.ts
+++ frontend/angular-ecommerce/src/app/service/cart.service.ts
@@ -11,7 +11,18 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() {}
+  // storage: Storage = sessionStorage;
+  storage: Storage = localStorage;
+
+  constructor() {
+    //read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+    if (data != null) {
+      this.cartItems = data;
+      //compute totals based on the data that is read from storage
+      this.computeCartTotal();
+    }
+  }
 
   addToCart(theCartItem: CartItem) {
     //check if we already have the item in our cart
@@ -52,6 +63,8 @@ export class CartService {
 
     //log cart data just for debugging purpose
     this.logCartData(totalPriceValue, totalQuanityValue);
+    //persist the cart data;
+    this.persistCartItems();
   }
 
   logCartData(totalPriceValue: number, totalQuanityValue: number) {
@@ -88,5 +101,9 @@ export class CartService {
       this.cartItems.splice(itemIndex, 1);
       this.computeCartTotal();
     }
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 }
